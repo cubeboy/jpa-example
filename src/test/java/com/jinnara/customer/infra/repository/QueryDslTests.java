bec26dd5f74ee8bad64cb5d7a9fc1e11a37d6526
KIM JIN <cubeboy.kim@gmail.com>
2023-09-04 00:33:38 +0900

    add querydsl Factory

--- src/test/java/com/jinnara/customer/infra/repository/QueryDslTests.java
+++ src/test/java/com/jinnara/customer/infra/repository/QueryDslTests.java
@@ -1,14 +1,19 @@
 package com.jinnara.customer.infra.repository;
 
+import com.jinnara.customer.config.QueryDslConfig;
 import com.jinnara.customer.domain.model.Member;
 import com.jinnara.customer.domain.model.QMember;
+
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -17,11 +22,18 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@DataJpaTest
+@DataJpaTest(includeFilters = {
+    @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = {QueryDslConfig.class})
+})
 @ExtendWith(SpringExtension.class)
-public class QueryDslTests {
+public final class QueryDslTests {
   @Autowired
   EntityManager entityManager;
+
+  @Autowired
+  JPAQueryFactory factory;
 
   @Test
   @Sql("/insertMember.sql")
@@ -36,6 +48,22 @@ public class QueryDslTests {
                 qMember.name,
                 qMember.gradeCode)
         )
+        .from(qMember)
+        .where(qMember.name.likeIgnoreCase(("user%")))
+        .fetch();
+    assertEquals(20, members.size());
+  }
+
+  @Test
+  @Sql("/insertMember.sql")
+  public void queryFactorySelect() {
+    QMember qMember = QMember.member;
+    List<Member> members = factory
+        .select(
+            Projections.fields(Member.class,
+                qMember.memberId,
+                qMember.name,
+                qMember.gradeCode))
         .from(qMember)
         .where(qMember.name.likeIgnoreCase(("user%")))
         .fetch();
